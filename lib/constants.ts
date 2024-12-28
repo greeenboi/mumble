8ca78581082c45737c7c2855fbc8090588d5ba6c
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-12-28 20:22:08 +0530

    added nativewind and react native reusables

---
+++ lib/constants.ts
@@ -0,0 +1,18 @@
+export const NAV_THEME = {
+  light: {
+    background: 'hsl(296 100% 95%)', // background
+    border: 'hsl(296 30% 50%)', // border
+    card: 'hsl(296 50% 90%)', // card
+    notification: 'hsl(0 100% 30%)', // destructive
+    primary: 'hsl(296 15% 39.2%)', // primary
+    text: 'hsl(296 5% 10%)', // foreground
+  },
+  dark: {
+    background: 'hsl(296 50% 10%)', // background
+    border: 'hsl(296 30% 26%)', // border
+    card: 'hsl(296 50% 10%)', // card
+    notification: 'hsl(0 100% 30%)', // destructive
+    primary: 'hsl(296 15% 39.2%)', // primary
+    text: 'hsl(296 5% 90%)', // foreground
+  },
+};
